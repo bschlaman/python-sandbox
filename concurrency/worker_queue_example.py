1cd65b863d6903e1bf1473ef23e939e8c1cfb139
Brendan Schlaman <brendan.schlaman@gmail.com>
2023-03-23 21:42:05 -0700

    add some notes to the worker example

--- concurrency/worker_queue_example.py
+++ concurrency/worker_queue_example.py
@@ -2,17 +2,20 @@ import asyncio
 from utils import fetch_data_delayed_async_pprint, get_coro_name
 
 delays = [2, 1, 4, 2, 5, 1]
-rev_delays = list(reversed(delays))
 
 
 task_queue = asyncio.Queue()
 
 
+# The name "worker" is subtle.  Perhaps "scheduler" is more precise,
+# as it schedules the task for execution by awating it.
+# If in place of `await task` was `await asyncio.create_task(task)`,
+# the work would be scheduled on a different coroutine.
+# But because of `await task`, the work is done on the worker coroutine
 async def worker():
     print(f"worker started on coro: {get_coro_name()}")
     while True:
         task_id, task, stop_event = await task_queue.get()
-        # result = await asyncio.create_task(task)
         result = await task
         stop_event.set()
         print(f"[worker]: task: {task_id} result: {result['delay']}\n")
@@ -36,15 +39,14 @@ async def main_coroutine():
     for event in tasks.values():
         await event.wait()
 
-    # await asyncio.gather(*tasks)
-
 
 async def main():
     print(f"main started on coro: {get_coro_name()}")
+    # more worker Tasks can be created to speed up execution
     worker_task = asyncio.create_task(worker())
     main_task = asyncio.create_task(main_coroutine())
     await asyncio.gather(main_task)
-    # await task_queue.put((None, None, None))
+    # works without cancelling the worker, but I think it's good hygeine
     worker_task.cancel()
 
 
